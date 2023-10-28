57c4e19098d7e569553064153d56b5440bb128e8
SKarnjana <ksrisumrarn.pam@gmail.com>
2023-10-27 17:53:43 -0700

    thread card, comment, reply

--- lib/validations/thread.ts
+++ lib/validations/thread.ts
@@ -5,6 +5,6 @@ export const ThreadValidation = z.object({
   accountId: z.string(),
 });
 
-export const CoomentValidation = z.object({
+export const CommentValidation = z.object({
   thread: z.string().min(3, { message: "Minimum 3 characters" }),
 });
