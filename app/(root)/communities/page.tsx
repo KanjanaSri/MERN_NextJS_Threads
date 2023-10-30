861f18900c52c7eba85710bbda587a52f854f3fd
SKarnjana <ksrisumrarn.pam@gmail.com>
2023-10-29 21:00:33 -0700

    search functionality

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,34 +1,43 @@
-import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
+import { fetchUser } from "@/lib/actions/user.actions";
 import { fetchCommunities } from "@/lib/actions/community.actions";
-import CommunityCard from "@/components/cards/CommunityCard";
 
-async function Page() {
+import CommunityCard from "@/components/cards/CommunityCard";
+import Searchbar from "@/components/shared/Searchbar";
+import Pagination from "@/components/shared/Pagination";
+
+async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
   const user = await currentUser();
   if (!user) return null;
 
   const userInfo = await fetchUser(user.id);
-
   if (!userInfo?.onboarded) redirect("/onboarding");
 
   // Fetch communities
   const result = await fetchCommunities({
-    searchString: "",
-    pageNumber: 1,
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
     pageSize: 25,
   });
 
   return (
-    <section>
+    <>
       <h1 className="head-text mb-10">Communities</h1>
 
       {/* Search Bar */}
+      <div className="mt-5">
+        <Searchbar routeType="communities" />
+      </div>
 
-      <div className="mt-14 flex flex-col gap-9">
+      <section className="mt-9 flex flex-wrap justify-evenly gap-4">
         {result.communities.length === 0 ? (
-          <p className="no-result">No communities</p>
+          <p className="no-result">No Results</p>
         ) : (
           <>
             {result.communities.map((community) => (
@@ -44,8 +53,14 @@ async function Page() {
             ))}
           </>
         )}
-      </div>
-    </section>
+      </section>
+
+      <Pagination
+        path="communities"
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
+    </>
   );
 }
 
--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -3,21 +3,25 @@ import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 import UserCard from "@/components/cards/UserCard";
+import Searchbar from "@/components/shared/Searchbar";
+import Pagination from "@/components/shared/Pagination";
 
-async function Page() {
+async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
   const user = await currentUser();
-
   if (!user) return null;
 
   const userInfo = await fetchUser(user.id);
-
   if (!userInfo?.onboarded) redirect("/onboarding");
 
   // Fetch users
   const result = await fetchUsers({
     userId: user.id,
-    searchString: "",
-    pageNumber: 1,
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
     pageSize: 25,
   });
 
@@ -26,6 +30,7 @@ async function Page() {
       <h1 className="head-text mb-10">Search</h1>
 
       {/* Search Bar */}
+      <Searchbar routeType="search" />
 
       <div className="mt-14 flex flex-col gap-9">
         {result.users.length === 0 ? (
@@ -45,6 +50,12 @@ async function Page() {
           </>
         )}
       </div>
+
+      <Pagination
+        path="search"
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
     </section>
   );
 }
